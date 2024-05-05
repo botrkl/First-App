ddcd1f16cf9c446e06f5a16a9a5f51484614a415
botrkl <96987652+botrkl@users.noreply.github.com>
2024-05-05 08:36:19 +0300

    Created dbContext

---
+++ PersonalProjectManagementTool/PersonalProjectManagementTool.DAL/Context/BoardDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalProjectManagementTool.DAL.EntitesConfiguration;
+using PersonalProjectManagementTool.DAL.Entities;
+
+namespace PersonalProjectManagementTool.DAL.Context
+{
+    public class BoardDbContext : DbContext
+    {
+        public BoardDbContext(DbContextOptions options) : base(options) { }
+        
+        public DbSet<ListOfCards> ListsOfCards { get; set; }
+        public DbSet<ListOfCards> Cards { get; set; }
+        public DbSet<ListOfCards> CardsActivities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CardActivityConfiguration()); ;
+            modelBuilder.ApplyConfiguration(new CardConfiguration()); ;
+            modelBuilder.ApplyConfiguration(new ListOfCardsConfiguration()); ;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
