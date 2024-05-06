c0d2430973dbd3826d68e47238329976d1135667
botrkl <96987652+botrkl@users.noreply.github.com>
2024-05-06 04:57:33 +0300

    Added Swagger

--- PersonalProjectManagementTool/PersonalProjectManagementTool.API/Program.cs
+++ PersonalProjectManagementTool/PersonalProjectManagementTool.API/Program.cs
@@ -1,10 +1,26 @@
 using PersonalProjectManagementTool.DAL.Extensions;
 using PersonalProjectManagementTool.BLL.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(new ProducesAttribute("application/json"));
+    options.Filters.Add(new ConsumesAttribute("application/json"));
+});
+
 builder.Services.InjectDAL(builder.Configuration);
 builder.Services.InjectBLL();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+app.UseHsts();
+app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.Run();
